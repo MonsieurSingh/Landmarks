c48ca15690291193331c1b826e1e8b8f9663868d
Teghjyot <66196010+Teghjyot@users.noreply.github.com>
2021-01-20 21:49:26 +1100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Teghjyot Singh on 20/1/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
