0a53743328660666de741c4d4d9d44949d412e1f
Teghjyot <66196010+Teghjyot@users.noreply.github.com>
2021-01-20 22:57:05 +1100

    Handling User Input

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Teghjyot Singh on 20/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
